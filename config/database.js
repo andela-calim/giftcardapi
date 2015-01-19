083bb02cdec33dcab6b8b7267a06dfcab2d2dfff
Chiemeka Alim <lord.chiemy@gmail.com>
2015-01-19 18:16:34 +0100

    changing databases

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-	'url' : 'mongodb://db_user:giftcard-api@ds031601.mongolab.com:31601/giftcard-api',
-	// 'url' : 'mongodb://localhost/giftcard-apiDB',
+	'url' : 'mongodb://db_user:giftcardapidb@ds031751.mongolab.com:31751/giftcardapidb',
 	'JWT_SECRET' : 'lordchiemyofthemilkywaygalaxy'
 };
